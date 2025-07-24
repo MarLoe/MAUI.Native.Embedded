aea0b96ad33fc8e449ead752fce3269ad880df96
Martin Løbger <malo@bang-olufsen.dk>
2025-07-24 23:12:08 +0200

    Add ios xcode placeholder project

---
+++ MAUI.Native.Sample/Platforms/iOS/MAUI.Native.ios/MAUI.Native.ios/MAUI_Native_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAUI_Native_iosApp.swift
+//  MAUI.Native.ios
+//
+//  Created by Martin Løbger on 24/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MAUI_Native_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
