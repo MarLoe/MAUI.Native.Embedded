c07b4d91d80acbf0c3a7f5d782a0ce65934bafca
Martin Løbger <malo@bang-olufsen.dk>
2025-07-25 11:07:16 +0200

    Renaming the repo from MAUI.Native to MAUI.Native.Embedded

---
+++ MAUI.Native.Embedded.Sample/Platforms/iOS/MAUI.Native.Embedded.ios/MAUI.Native.Embedded.ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MAUI.Native.Embedded.ios
+//
+//  Created by Martin Løbger on 24/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, MAUI.Native.Embedded!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
